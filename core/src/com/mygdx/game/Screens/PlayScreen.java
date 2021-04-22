56d1f46afb37881f9cd46685a8af7a4540247dbf
D0niL <kdi190104@mail.ru>
2021-04-22 12:09:29 +0300

    Something went wrong

--- core/src/com/mygdx/game/Screens/PlayScreen.java
+++ core/src/com/mygdx/game/Screens/PlayScreen.java
@@ -72,9 +72,21 @@ public class PlayScreen implements Screen, InputProcessor {
 
     // STAGE------------------------------------------------
     private Stage stage;
+    private Stage stage_start;
+
+    private boolean stage_remembered;
+
     private Texture playbtn = new Texture("playbtn.png");
     private Texture settingsbtn = new Texture("settingsbtn.png");
     private Texture shopbtn = new Texture("shopbtn.png");
+
+    private Texture number1 = new Texture("numbers/one.png");
+    private Texture number2 = new Texture("numbers/two.png");
+    private Texture number3 = new Texture("numbers/three.png");
+    private Texture number4 = new Texture("numbers/four.png");
+    private Texture number5 = new Texture("numbers/five.png");
+    private Texture number6 = new Texture("numbers/six.png");
+    private Texture remembered = new Texture("badlogic.jpg");
 
     private Vector2 btnSize = new Vector2();
 
@@ -107,7 +119,7 @@ public class PlayScreen implements Screen, InputProcessor {
     public void show() {
 
         intersection = new Vector3();
-//        boundingBoxes.add(new BoundingBox(new Vector3(.5f, .5f, .5f), new Vector3(-.5f, -.5f, -.5f)));
+        boundingBoxes.add(new BoundingBox(new Vector3(.5f, .5f, .5f), new Vector3(-.5f, -.5f, -.5f)));
         boundingBoxes.add(new BoundingBox(new Vector3(.5f, .6f, .5f), new Vector3(.6f, -.6f, -.5f)));
         boundingBoxes.add(new BoundingBox(new Vector3(-.5f, .6f, .5f), new Vector3(-.6f, -.6f, -.5f)));
         boundingBoxes.add(new BoundingBox(new Vector3(-.5f, .6f, .5f), new Vector3(.5f, .5f, -.5f)));
@@ -116,7 +128,7 @@ public class PlayScreen implements Screen, InputProcessor {
         boundingBoxes.add(new BoundingBox(new Vector3(-.5f, -.5f, -.5f), new Vector3(.5f, .5f, -.6f)));
 
         // RANDOM--------------------------------------
-        List<Integer> arr = new ArrayList<>();
+        final List<Integer> arr = new ArrayList<>();
 //        ArrayList Location_Images = new ArrayList();
         Location_Images.add(-1);
         Location_Images.add("Cubes/one.png");
@@ -158,52 +170,137 @@ public class PlayScreen implements Screen, InputProcessor {
         mbatch = new ModelBatch();
         // ENVIRONMENT--------------------------------------
 
+        // D3DJ ------------------------------------------------------
+        G3dModelLoader modelLoader = new G3dModelLoader(new JsonReader());
+        Model model1 = modelLoader.loadModel(Gdx.files.getFileHandle("Cubes/Ready.g3dj", Files.FileType.Internal));
+        ModelInstance modelInstance1 = new ModelInstance(model1);
+        instances.add(modelInstance1);
+
+        for (int i = 1; i <= 6; i++) {
+            instances.get(0).getMaterial("Mat" + arr.get(i)).clear();
+            instances.get(0).getMaterial("Mat" + arr.get(i)).set(new TextureAttribute(TextureAttribute.Diffuse, new Texture("" + Location_Images.get(i))));
+        }
+        instances.get(0).getNode("Cube").getChild(5);
+        // D3DJ ------------------------------------------------------
+
         //STAGE----------------------------------------------
         stage = new Stage();
-        Button startBtn = new Button(new SpriteDrawable(new Sprite(playbtn)));
-        Button settingsBtn = new Button(new SpriteDrawable(new Sprite(settingsbtn)));
-        Button shopBtn = new Button(new SpriteDrawable(new Sprite(shopbtn)));
+        stage_start = new Stage();
+        stage_remembered = false;
+        Button number_1 = new Button(new SpriteDrawable(new Sprite(number1)));
+        Button number_2 = new Button(new SpriteDrawable(new Sprite(number2)));
+        Button number_3 = new Button(new SpriteDrawable(new Sprite(number3)));
+        Button number_4 = new Button(new SpriteDrawable(new Sprite(number4)));
+        Button number_5 = new Button(new SpriteDrawable(new Sprite(number5)));
+        Button number_6 = new Button(new SpriteDrawable(new Sprite(number6)));
 
-        btnSize = new Vector2(178, 178);
+        Button Remembered = new Button(new SpriteDrawable(new Sprite(remembered)));
 
+        btnSize = new Vector2(140, 140);
         number = 1;
 
-        startBtn.setBounds((MainC.WIDTH / 2) - playbtn.getWidth() / 2, MainC.HEIGHT / 7 * 2, btnSize.x, btnSize.y);
-        settingsBtn.setBounds(MainC.WIDTH / 2 - settingsbtn.getWidth() + playbtn.getWidth() / 2, MainC.HEIGHT / 7 * 2 - settingsbtn.getHeight(), 78, 78);
+        number_1.setBounds(MainC.WIDTH / 2 - btnSize.x / 2 - btnSize.x - 20, MainC.HEIGHT / 9 * 5, btnSize.x, btnSize.y);
+        number_2.setBounds(MainC.WIDTH / 2 - btnSize.x / 2 , MainC.HEIGHT / 9 * 5, btnSize.x, btnSize.y);
+        number_3.setBounds(MainC.WIDTH / 2 - btnSize.x / 2  + btnSize.x + 20, MainC.HEIGHT / 9 * 5, btnSize.x, btnSize.y);
+
+        number_4.setBounds(MainC.WIDTH / 2 - btnSize.x / 2 - btnSize.x - 20, MainC.HEIGHT / 9 * 5 - btnSize.y - 20, btnSize.x, btnSize.y);
+        number_5.setBounds(MainC.WIDTH / 2 - btnSize.x / 2 , MainC.HEIGHT / 9 * 5 - btnSize.y - 20, btnSize.x, btnSize.y);
+        number_6.setBounds(MainC.WIDTH / 2 - btnSize.x / 2 + btnSize.x + 20, MainC.HEIGHT / 9 * 5 - btnSize.y - 20, btnSize.x, btnSize.y);
+
+        Remembered.setBounds(MainC.WIDTH / 2 - btnSize.x / 2, 30, btnSize.x, btnSize.y);
+
+
+        //startBtn.setBounds((MainC.WIDTH / 2) - playbtn.getWidth() / 2, MainC.HEIGHT / 7 * 2, btnSize.x, btnSize.y);
+        //settingsBtn.setBounds(MainC.WIDTH / 2 - settingsbtn.getWidth() + playbtn.getWidth() / 2, MainC.HEIGHT / 7 * 2 - settingsbtn.getHeight(), 78, 78);
 
         final InputMultiplexer multiplexer = new InputMultiplexer();
 
-        settingsBtn.addListener(new ClickListener() {
-            @Override
+        number_1.addListener(new ClickListener(){
+            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+                if(!Touched)return;
+                Touched = false;
+                number = 1;
+                closest.parts.get(0).material.set(new TextureAttribute(TextureAttribute.Diffuse, new Texture("" + Location_Images.get(number))));
+                Gdx.input.setInputProcessor(multiplexer);
+                super.touchUp(event, x, y, pointer, button);
+            }
+        });
+        number_2.addListener(new ClickListener(){
+            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+                if(!Touched)return;
+                Touched = false;
+                number = 2;
+                closest.parts.get(0).material.set(new TextureAttribute(TextureAttribute.Diffuse, new Texture("" + Location_Images.get(number))));
+                Gdx.input.setInputProcessor(multiplexer);
+                super.touchUp(event, x, y, pointer, button);
+            }
+        });
+        number_3.addListener(new ClickListener(){
             public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
                 if(!Touched)return;
                 Touched = false;
                 number = 3;
                 closest.parts.get(0).material.set(new TextureAttribute(TextureAttribute.Diffuse, new Texture("" + Location_Images.get(number))));
-                WindowIsOpen = false;
                 Gdx.input.setInputProcessor(multiplexer);
                 super.touchUp(event, x, y, pointer, button);
             }
         });
-        startBtn.addListener(new ClickListener() {
-            @Override
+        number_4.addListener(new ClickListener(){
             public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
                 if(!Touched)return;
                 Touched = false;
                 number = 4;
                 closest.parts.get(0).material.set(new TextureAttribute(TextureAttribute.Diffuse, new Texture("" + Location_Images.get(number))));
-                WindowIsOpen = false;
                 Gdx.input.setInputProcessor(multiplexer);
                 super.touchUp(event, x, y, pointer, button);
+            }
+        });
+        number_5.addListener(new ClickListener(){
+            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+                if(!Touched)return;
+                Touched = false;
+                number = 5;
+                closest.parts.get(0).material.set(new TextureAttribute(TextureAttribute.Diffuse, new Texture("" + Location_Images.get(number))));
+                Gdx.input.setInputProcessor(multiplexer);
+                super.touchUp(event, x, y, pointer, button);
+            }
+        });
+        number_6.addListener(new ClickListener(){
+            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+                if(!Touched)return;
+                Touched = false;
+                number = 6;
+                closest.parts.get(0).material.set(new TextureAttribute(TextureAttribute.Diffuse, new Texture("" + Location_Images.get(number))));
+                Gdx.input.setInputProcessor(multiplexer);
+                super.touchUp(event, x, y, pointer, button);
+            }
+        });
+        Remembered.addListener(new ClickListener(){
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                stage_remembered = true;
+                System.out.println("oooo");
+                for (int i = 1; i <= 6; i++) {
+                    instances.get(0).getMaterial("Mat" + arr.get(i)).clear();
+                    //instances.get(0).getMaterial("Mat" + arr.get(i)).set(new TextureAttribute(TextureAttribute.Diffuse, new Texture("" + Location_Images.get(i))));
+                    //System.out.println(arr);
+                }
+                //multiplexer.addProcessor(PlayScreen.this);
+                multiplexer.removeProcessor(stage_start);
+                Gdx.input.setInputProcessor(multiplexer);
+                return super.touchDown(event, x, y, pointer, button);
             }
 
         });
 
-        stage.addActor(settingsBtn);
-        stage.addActor(startBtn);
-
+        stage.addActor(number_1);
+        stage.addActor(number_2);
+        stage.addActor(number_3);
+        stage.addActor(number_4);
+        stage.addActor(number_5);
+        stage.addActor(number_6);
+        stage_start.addActor(Remembered);
         //STAGE----------------------------------------------
-
 
         // CAMERA--------------------------------------
         camera = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
@@ -214,27 +311,14 @@ public class PlayScreen implements Screen, InputProcessor {
 
         controll = new CameraInputController(camera);
         controll.autoUpdate = true;
-
         multiplexer.addProcessor(this);
 //        multiplexer.addProcessor(stage);
         multiplexer.addProcessor(controll);
+        multiplexer.addProcessor(stage_start);
 
         Gdx.input.setInputProcessor(multiplexer);
         // CAMERA--------------------------------------
-        // D3DJ ------------------------------------------------------
-        G3dModelLoader modelLoader = new G3dModelLoader(new JsonReader());
-        Model model1 = modelLoader.loadModel(Gdx.files.getFileHandle("Cubes/Ready.g3dj", Files.FileType.Internal));
-        ModelInstance modelInstance1 = new ModelInstance(model1);
-        instances.add(modelInstance1);
 
-        for (int i = 1; i <= 6; i++) {
-
-            instances.get(0).getMaterial("Mat" + arr.get(i)).clear();
-            instances.get(0).getMaterial("Mat" + arr.get(i)).set(new TextureAttribute(TextureAttribute.Diffuse, new Texture("" + Location_Images.get(i))));
-            System.out.println(arr);
-        }
-        instances.get(0).getNode("Cube").getChild(5);
-        // D3DJ ------------------------------------------------------
 
         box = new BoundingBox();
     }
@@ -250,8 +334,11 @@ public class PlayScreen implements Screen, InputProcessor {
         mbatch.begin(camera);
         mbatch.render(instances, env);
         mbatch.end();
+        if(!stage_remembered){
+            stage_start.act();
+            stage_start.draw();
+        }
         if (Touched) {
-            WindowIsOpen = true;
             stage.act();
             stage.draw();
         }
@@ -310,16 +397,18 @@ public class PlayScreen implements Screen, InputProcessor {
 
     @Override
     public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-        if (isDragged || WindowIsOpen || Touched) return false;
+        if (isDragged || Touched) return false;
 
         Ray ray = camera.getPickRay(screenX, screenY);
         count += 1;
+        System.out.println(count);
         ArrayList<Node> nodes = new ArrayList<>();
         for (int i = 0; i < instances.get(0).getNode("Cube").getChildCount(); i++) {
             Node node = instances.get(0).getNode("Cube").getChild(i);
             node.calculateBoundingBox(box);
             if (Intersector.intersectRayBoundsFast(ray, box)) {
                 nodes.add(node);
+                System.out.println("Done");
                 NodeDone = true;
             }
         }
